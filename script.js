4ca99bbe620612687d71b2283ecaaa775107c818
Cmellons <codymellons@yahoo.com>
2024-03-13 17:13:51 -0400

    Working imgloader

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-// API_KEy
-// https://api.giphy.com/v1/gifs/search?api_key=SbwrnGqBbHSNIRmqVhPBCILWXxrqzUyj&q=ryan+gosling&limit=12
-// https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWttZXc3enI4bGlueWFxN2djd3MzNWZ6eG4zZGhqYmludWRjeXpneCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LYOkCQc638NCaROTNy/giphy.gif"
 
 searchBTN = document.querySelector('.search')
 inpTAG = document.querySelector('.inpSearch')
 row1 = document.querySelector('.row1')
+let loadCountImage = 1
+function generateGifHTML(result) {
 
-
-function generateGifHTML(result){
-console.log(result)
-
-result.data.forEach(function(onebyone){
-    html = '';
-    html += `
+    result.data.forEach(function (onebyone) {
+        loadCountImage++
+        html = '';
+        html += `
 <div class ="col1">
     <img src="${onebyone.images.original.url}" width='250' height='190'>
-    <a href="${onebyone.images.original.url}" download>Download Gif</a>
+ 
+    <a href="${onebyone.images.original.url}" class="" download>Download<span>${((onebyone.images.original.size / 8) / (1024 * 1024)).toFixed(4)}(MB)</span>  </a>
     </div>
     `
 
+        row1.innerHTML += html;
+        if (loadCountImage === 12) {
+            console.log(loadCountImage)
+            hideLoader()
+        }
+    });
 
-console.log(html)
+    allImages = document.querySelectorAll('.row1 img')
 
-    // console.log(onebyone.images.original.url);
-    // console.log(onebyone.images.original.size);
-    // 
-    row1.innerHTML += html;
-});
+
 
 
 }
-searchBTN.addEventListener('click',function () {
-url = `https://api.giphy.com/v1/gifs/search?q=${inpTAG.value}&api_key=SbwrnGqBbHSNIRmqVhPBCILWXxrqzUyj&limit=12`
-fetch(url).then(function(response){
-    return response.json();
-}) .then(function(data){
-    fetchedData = data;
-    generateGifHTML(fetchedData)
-    return data;
-}) .catch(function(error) {
-      console.log('Error:', error)
-});
+
+
+searchBTN.addEventListener('click', function () {
+    url = `https://api.giphy.com/v1/gifs/search?q=${inpTAG.value}&api_key=SbwrnGqBbHSNIRmqVhPBCILWXxrqzUyj&limit=12`
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        fetchedData = data;
+        showLoader()
+        setTimeout(() => {
+            generateGifHTML(fetchedData)
+
+        }, 1000);
+
+        return data;
+    }).catch(function (error) {
+        console.log('Error:', error);
+    });
 })
+
+function showLoader() {
+    document.querySelector('.loader').classList.remove('loaded')
+}
+
+function hideLoader() {
+    document.querySelector('.loader').classList.add('loaded')
+}
